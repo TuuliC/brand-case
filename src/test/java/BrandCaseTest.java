c130472406bf8b659030d2cda41f508175af17c6
tuuli <tuuli@tu>
2023-01-11 22:35:19 +0800

    test 中文

---
+++ src/test/java/BrandCaseTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+/**
+ * @author tuuli
+ * @time Created in 2023/1/11 22:31
+ * @description
+ */
+public class BrandCaseTest {
+    @Test
+    public void test(){
+
+    }
+
+}
